fa4ad1ff74e23f2bef5f651fc8216aa454c09edf
Luiz Kauã <lkaua4546@gmail.com>
2026-08-13 16:56:30 -0300

    Subindo tudo no github para que possa fazer outras alterações

--- src/app/component/menu-component/menu-component.component.ts
+++ src/app/component/menu-component/menu-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-menu-component',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-component.component.html',
   styleUrl: './menu-component.component.css'
 })
