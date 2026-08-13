9900d3d7d05976c21f498a411a53e16142823782
Luiz Kauã <lkaua4546@gmail.com>
2026-08-13 16:55:18 -0300

    Adicionado as rotas das páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AtletaComponent } from './component/atleta/atleta.component';
+import { HomeComponent } from './component/home/home.component';
+export const routes: Routes = [
+    {
+       path:'',
+       redirectTo:'/home',
+       pathMatch:'full'
+    },
+    {
+        path:'home',
+        component:HomeComponent,
+    },
+    {
+        path:'cadastroAtleta',
+        component:AtletaComponent
+    }
+];
