f0902b0dfae82591b21e91dbb8411f1d8997bf74
Luiz Kauã <lkaua4546@gmail.com>
2026-08-13 15:52:21 -0300

    Importando e exportando o atletaComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MenuComponentComponent } from './component/menu-component/menu-component.component';
+import { AtletaComponent } from './component/atleta/atleta.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponentComponent,AtletaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
