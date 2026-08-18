5e4ba27c638e9771b9d6dc73ed7ea43e93bf57e6
Luiz Kauã <lkaua4546@gmail.com>
2026-08-18 16:12:08 -0300

    Criada a lógica da página listar para que ela possa listar todos os atletas cadastrados

--- src/app/component/listar-atleta/listar-atleta.component.ts
+++ src/app/component/listar-atleta/listar-atleta.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Atleta } from '../../models/Atleta';
+import { AtletaServiceService } from '../../service/atleta-service.service';
+import { Router } from '@angular/router';
+import{ signal} from '@angular/core'
 
 @Component({
   selector: 'app-listar-atleta',
@@ -9,4 +13,45 @@ import { Component } from '@angular/core';
 })
 export class ListarAtletaComponent {
 
+  //listaAtletas: Atleta [] = []
+  listaAtletas = signal<Atleta[]>([]);
+
+  constructor(private listaService: AtletaServiceService, private router: Router) {}
+
+  ngOnInit(){
+    this.listar()
+  }
+
+  listar() {
+    this.listaService.listarAtletas().subscribe({
+      next:(dadosAtletas) => {
+        //this.listaAtletas = [...dadosAtletas].sorte((a,b) => a.nome.localeCompare(b.nome))
+        this.listaAtletas.set([...dadosAtletas].sort((a,b) => a.nome.localeCompare(b.nome)))
+
+        console.table(this.listaAtletas())
+      },
+      error: (msgErro) => {
+        console.log("Erro ao listar Atletas ",msgErro)
+      }
+    })
+  }
+
+  excluir(id:number) {
+    if(confirm("Deseja Excluir o Atleta")) {
+      this.listaService.excluirAtleta(id).subscribe({
+        next: (resposta) => {
+          console.log("Excluído com Sucesso !!! ", resposta)
+
+          this.listar()
+        },
+        error: (msgErro) => {
+          console.log("Erro ao listar Atletas ",msgErro)
+        }
+      })
+    }
+  }
+
+  carregaDadosAtletaForm(atleta:Atleta) {
+    this.router.navigate(['/cadastroAtleta', atleta.id])
+  }
 }
