bbfd69d2696f31d634f27905694ee3bda282337e
Luiz Kauã <lkaua4546@gmail.com>
2026-08-19 15:46:44 -0300

    Adicionado comentários e adicionado nova função de alterar os dados cadastrados

--- src/app/component/atleta/atleta.component.ts
+++ src/app/component/atleta/atleta.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AtletaServiceService } from '../../service/atleta-service.service';
 import { Atleta } from '../../models/Atleta';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-atleta',
@@ -10,6 +11,7 @@ import { Atleta } from '../../models/Atleta';
   templateUrl: './atleta.component.html',
   styleUrl: './atleta.component.css'
 })
+
 export class AtletaComponent {
   //Declarando Atributos
   nome = ''
@@ -21,8 +23,11 @@ export class AtletaComponent {
   cidade = ''
   uf = ''
 
+  idAtleta = 0
+  editar = false
+
   //Declaração do construtor
-  constructor(private atletaService: AtletaServiceService) { }
+  constructor(private atletaService: AtletaServiceService, private http: ActivatedRoute) { }
 
   //Declaração de funções 
   exibirDados() {
@@ -30,6 +35,17 @@ export class AtletaComponent {
     this.limparDados()
   }
 
+  //Inicializa automaticamente ao carregar o componente
+  ngOnInit() {
+    this.idAtleta = Number(this.http.snapshot.paramMap.get('id'))
+
+    if(this.idAtleta > 0){
+      this.editar = true
+      this.carregaDados(this.idAtleta)
+    }
+  }
+
+  //Função para limpar todos os dados quando apertar o botão limpar
   limparDados() {
     this.nome = ''
     this.cpf = 0
@@ -41,6 +57,28 @@ export class AtletaComponent {
     this.uf = ''
   }
 
+  //Função para de carregar todos os dados cadastrados 
+carregaDados(idAtleta: number){
+  this.atletaService.listarAtleta(idAtleta).subscribe({
+    next:(dadosAtleta) => {
+       this.nome = dadosAtleta.nome
+       this.cpf = dadosAtleta.cpf
+       this.sexo = dadosAtleta.sexo
+       this.cep = dadosAtleta.cep
+       this.ruaLogradouro = dadosAtleta.ruaLogradouro
+       this.bairro = dadosAtleta.bairro
+       this.cidade = dadosAtleta.cidade
+       this.uf = dadosAtleta.uf
+
+
+    },
+    error:(msgErro) =>{
+      console.log('Erro ao Listar Atleta', msgErro)
+    }
+  })
+}
+
+  //Função para que envie os dados cadastrados para a que possa listar
   enviarDadosAtletas(){
     const atleta = new Atleta()
     atleta.nome = this.nome
@@ -52,15 +90,31 @@ export class AtletaComponent {
     atleta.cidade = this.cidade
     atleta.uf = this.uf
 
-    this.atletaService.salvarAtleta(atleta).subscribe({
-      next: (resposta) =>{
-        console.log(resposta)
-      },
-      error:(msgErro) => {
-        console.log(msgErro)
-      }
-    })
+    if(this.editar) {
+      atleta.id = this.idAtleta
+
+      this.atletaService.salvarAtleta(atleta).subscribe({
+        next: (resposta) =>{
+          console.log(resposta)
+        },
+        error: (msgErro) => {
+          console.log(msgErro)
+        }
+      })
+    } else {
+      this.atletaService.salvarAtleta(atleta).subscribe({
+        next: (resposta) =>{
+          console.log(resposta)
+        },
+        error: (msgErro) => {
+          console.log(msgErro)
+        }
+      })
+      
+    }
+
     
+    //Chamando função para que limpe os dados quando cadastrar
     this.limparDados()
 
     this.atletaService.listarAtletas()
