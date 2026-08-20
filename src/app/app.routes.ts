664b85fccee051f58cb853a3c9172b9d9d29a810
Luiz Kauã <lkaua4546@gmail.com>
2026-08-20 15:04:42 -0300

    Adicionado 2 links para o listar corrida e listar corrida com o parâmetro do id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { AtletaComponent } from './component/atleta/atleta.component';
 import { HomeComponent } from './component/home/home.component';
 import { CorridaComponent } from './component/corrida/corrida.component';
 import { ListarAtletaComponent } from './component/listar-atleta/listar-atleta.component';
+import { ListarCorridaComponent } from './component/listar-corrida/listar-corrida.component';
+
 export const routes: Routes = [
     {
        path:'',
@@ -26,7 +28,15 @@ export const routes: Routes = [
         component:CorridaComponent
     },
     {
+        path:'cadastroCorridas/:id',
+        component:CorridaComponent
+    },
+    {
         path:'listar-atleta',
         component:ListarAtletaComponent
+    },
+    {
+        path:'corridas',
+        component:ListarCorridaComponent
     }
 ];
