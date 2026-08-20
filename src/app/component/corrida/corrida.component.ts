021f5a2ecbdbc426cff1058bc3204d29fcae9a27
Luiz Kauã <lkaua4546@gmail.com>
2026-08-20 15:05:55 -0300

    Adicionando ao função para alterar e excluir

--- src/app/component/corrida/corrida.component.ts
+++ src/app/component/corrida/corrida.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Corrida } from '../../models/Corrida';
 import { CorridaServiceService } from '../../service/corrida-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-corrida',
@@ -11,30 +12,70 @@ import { CorridaServiceService } from '../../service/corrida-service.service';
   styleUrl: './corrida.component.css'
 })
 export class CorridaComponent {
-   id = ''
    descricao = ''
    data = ''
    distancia = ''
 
-   constructor(private corridaService: CorridaServiceService) {
-   }
-   corridaDados(){
-    console.log(this.descricao, this.data, this.distancia)
+   idCorrida = 0
+   editarCorrida = false
+   constructor(private corridaService: CorridaServiceService, private http: ActivatedRoute) {
    }
 
+   ngOnInit() {
+    this.idCorrida = Number(this.http.snapshot.paramMap.get('id'))
+
+    if(this.idCorrida > 0){
+      this.editarCorrida = true
+      this.corridaDados(this.idCorrida)
+    }
+   }
+   
    limparCorrida(){
     this.descricao = ''
     this.data = ''
     this.distancia = ''
    }
    
+   corridaDados(idCorrida:number){
+    this.corridaService.listarCorrida(idCorrida).subscribe({
+      next:(dadosCorrida) => {
+        this.descricao = dadosCorrida.descricao
+        this.data = dadosCorrida.data
+        this.distancia = dadosCorrida.distancia
+      },
+      error:(msgErro) =>{
+        console.log('Erro ao Listar Corrida',msgErro)
+      }
+    })
+   }  
+
    salvarCorrida (){
     const corrida = new Corrida()
     corrida.descricao = this.descricao
     corrida.data = this.data
     corrida.distancia = this.distancia
 
-    this.corridaService.adicionarCorrida(corrida)
+    if(this.editarCorrida) {
+      corrida.id = this.idCorrida
+
+      this.corridaService.adicionarCorrida(corrida).subscribe({
+        next:(resposta) =>{
+          console.log(resposta)
+        },
+        error: (msgErro) => {
+          console.log(msgErro)
+        }
+      })
+    } else{
+      this.corridaService.adicionarCorrida(corrida).subscribe({
+        next: (resposta) =>{
+          console.log(resposta)
+        },
+        error: (msgErro) => {
+          console.log(msgErro)
+        }
+      })
+    }
     this.limparCorrida()
 
     this.corridaService.listarCorridas()
