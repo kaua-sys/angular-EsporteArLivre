7fcba8d828489e138883698abc3ffbb81f326ead
Luiz Kauã <lkaua4546@gmail.com>
2026-08-21 15:34:54 -0300

    Criado componente incrições

---
+++ src/app/component/inscricoes/inscricoes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscricoes',
+  standalone: true,
+  imports: [],
+  templateUrl: './inscricoes.component.html',
+  styleUrl: './inscricoes.component.css'
+})
+export class InscricoesComponent {
+
+}
