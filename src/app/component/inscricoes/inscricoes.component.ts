96cedbc880e0c590f3a70e57d0cb2f544420b22f
Luiz Kauã <lkaua4546@gmail.com>
2026-08-24 13:34:42 -0300

    Criando lógica para a página inscrições

--- src/app/component/inscricoes/inscricoes.component.ts
+++ src/app/component/inscricoes/inscricoes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CorridaServiceService } from '../../service/corrida-service.service';
+import { AtletaServiceService } from '../../service/atleta-service.service';
 
 @Component({
   selector: 'app-inscricoes',
@@ -8,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './inscricoes.component.css'
 })
 export class InscricoesComponent {
+  atleta = ''
+  cpf = 0
+  corrida = ''
+  distancia5 = ''
+  distancia10 = ''
+  distancia25 = ''
+  tamanho = ''
+  categoria = ''
+
 
 }
