992e6c2cf42c6b87170ac579981e9dd5717fffb9
Luiz Kauã <lkaua4546@gmail.com>
2026-09-03 15:16:00 -0300

    Novas declarações ao model para que coincida com o do backend

--- src/app/models/Atleta.ts
+++ src/app/models/Atleta.ts
@@ -2,10 +2,12 @@ export class Atleta{
     id: number = 0
     nome: string =''
     cpf: number = 0
-    data: string = ''
+    data_nascimento: string = ''
+    peso:number = 0
+    altura:number = 0
     sexo: string = ''
     cep: number = 0
-    ruaLogradouro: string = ''
+    rua_logradouro: string = ''
     bairro:  string = ''
     cidade: string = ''
     uf: string = ''
