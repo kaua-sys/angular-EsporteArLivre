b7d091a64786aa856a47bc84598dcb0d95bb683b
Luiz Kauã <lkaua4546@gmail.com>
2026-09-03 15:04:28 -0300

    Adicionado duas funções uma para calcular o imc e outra para classificar como peso,sobrepeso

--- src/app/service/atleta-service.service.ts
+++ src/app/service/atleta-service.service.ts
@@ -45,31 +45,36 @@ export class AtletaServiceService {
 
   //Listar elementos
   listarAtletas(): Observable<Atleta[]> {
-    const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta`
+    //const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta`
+    const urlApi = `http://127.0.0.1:8000/pessoa/`
     return this.http.get<Atleta[]>(urlApi);
   }
 
   //Listar elemento
   listarAtleta(idAtleta: number): Observable<Atleta>{
-    const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta/${idAtleta}`
+    //const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta/${idAtleta}`
+    const urlApi = `http://127.0.0.1:8000/pessoa/${idAtleta}`
     return this.http.get<Atleta>(urlApi)
   }
 
   //Adicionando elemento
   salvarAtleta(atleta:Atleta): Observable<Atleta> {
-    const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta`
+    //const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta`
+    const urlApi = `http://127.0.0.1:8000/pessoa/`
     return this.http.post<Atleta>(urlApi,atleta)
   }
 
   //Remover elemento
   excluirAtleta(idAtleta:number): Observable<Atleta> {
-    const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta/${idAtleta}`
+    //const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta/${idAtleta}`
+    const urlApi = `http://127.0.0.1:8000/pessoa/${idAtleta}`
     return this.http.delete<Atleta>(urlApi)
   }
 
   //Atlrerando elemento do Array
   alterarAtleta(atleta: Atleta): Observable<Atleta> {
-    const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta/${atleta.id}`
+    //const urlApi = `https://6a835a84cb486d243403a3f7.mockapi.io/esportearLivre/Atleta/${atleta.id}`
+    const urlApi = `http://127.0.0.1:8000/pessoa/${atleta.id}`
     return this.http.put<Atleta>(urlApi,atleta)
   }
   calcularIdade(dataNascimento: string): number{
@@ -83,6 +88,32 @@ export class AtletaServiceService {
     idade--
   }
   return idade
+    
+  }
+  
+  imc(peso: number, altura: number) {
+    const Peso = peso
+    const Altura = altura
+
+    const imc  = Peso/(Altura*altura)
+
+    return Number(imc.toFixed(2))
+  }
+  
+  classificacaoImc(imc: number): string {
+
+    if(imc < 18.5) {
+      return "Abaixo do peso"
+    } else if (imc >= 18.5 && imc < 25){
+      return "Peso normal"
+    } else if (imc >= 25 && imc < 30) {
+      return "Sobrepeso"
+    } else if (imc >= 30 && imc < 35) {
+      return "Obesidade grau 1"
+    } else if (imc >= 35 && imc < 40) {
+      return "Obesidade grau 2"
+    }
+      return "Obesidade grau 3"
     
   }
 }
